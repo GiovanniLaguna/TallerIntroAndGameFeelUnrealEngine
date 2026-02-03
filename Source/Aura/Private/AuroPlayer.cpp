4c939fae2eecb2d639031d428916260055b5f27b
GiovanniLaguna <erick.laguna@saei.mx>
2026-02-02 23:56:24 -0600

    git ignore and Move

--- Source/Aura/Private/AuroPlayer.cpp
+++ Source/Aura/Private/AuroPlayer.cpp
@@ -7,11 +7,15 @@
 
 AAuroPlayer::AAuroPlayer()
 {
+
+//Rotacion del personaje
 	GetCharacterMovement()->bOrientRotationToMovement = true;
 	GetCharacterMovement()->RotationRate = FRotator(0.f,400.f,0.f);
 	GetCharacterMovement()->bConstrainToPlane = true;
 	GetCharacterMovement()->bSnapToPlaneAtStart = true;
 	
+
+//Rotacion de eje cancelada
 	bUseControllerRotationPitch = false;
 	bUseControllerRotationRoll = false;
 	bUseControllerRotationYaw = false;
