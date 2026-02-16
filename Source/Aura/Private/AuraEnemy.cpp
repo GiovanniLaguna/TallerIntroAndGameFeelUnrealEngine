780db959bbc402220a6e097b6516efc0f2306945
GiovanniLaguna <erick.laguna@saei.mx>
2026-02-15 19:04:13 -0600

    Post-Processing

--- Source/Aura/Private/AuraEnemy.cpp
+++ Source/Aura/Private/AuraEnemy.cpp
@@ -2,13 +2,23 @@
 
 
 #include "AuraEnemy.h"
+#include  "Aura/Aura.h"
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECC_Visibility, ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
-	bIsHighlighted = true;
+	GetMesh()->SetRenderCustomDepth(true);
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetRenderCustomDepth(true);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
-	bIsHighlighted = false;
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
 }
