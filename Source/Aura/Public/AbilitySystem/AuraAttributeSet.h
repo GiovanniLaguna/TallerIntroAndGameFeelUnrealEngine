e05bf90e3dee38395e1e10c04e875044826d7248
GiovanniLaguna <erick.laguna@saei.mx>
2026-02-17 00:17:26 -0600

    Mana & Health

--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -13,5 +13,33 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
+public: 
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
+	
+	UPROPERTY(BlueprintReadOnly ,Replicated = OnRep_Health, Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	
+	UPROPERTY(BlueprintReadOnly ,Replicated = OnRep_MaxHealth, Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	
+	UPROPERTY(BlueprintReadOnly ,Replicated = OnRep_Mana, Category = "Magic Attributes")
+	FGameplayAttributeData Mana;
+	
+	UPROPERTY(BlueprintReadOnly ,Replicated = OnRep_MaxMana, Category = "Magic Attributes")
+	FGameplayAttributeData MaxMana;
+	
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData& OldHealth) const;
+	
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth) const;
+	
+	UFUNCTION()
+	void OnRep_Mana(const FGameplayAttributeData& OldMana) const;
+	UFUNCTION()
+	void OnRep_MaxMana(const FGameplayAttributeData& OldMaxMana) const;
 	
 };
+
+
