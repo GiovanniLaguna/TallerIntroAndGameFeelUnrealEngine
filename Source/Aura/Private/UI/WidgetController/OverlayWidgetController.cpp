cdc47c0fdd626191d99c22a4f0e68f6d2b447af5
GiovanniLaguna <erick.laguna@saei.mx>
2026-03-03 22:50:13 -0600

    Entrega final

--- Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -1,6 +1,7 @@
 // Copyrigh Quetz-Lab 2026 
 
 #include "UI/WidgetController/OverlayWidgetController.h"
+#include  "UI/WidgetController/AuraWidgetController.h"
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystemComponent.h" // <-- ESTE INCLUDE ES VITAL PARA QUE FUNCIONE
 
@@ -14,6 +15,13 @@ void UOverlayWidgetController::BroadcastInitialValues()
 
 void UOverlayWidgetController::BindCallbacksToDependencies()
 {
+	Super::BindCallbacksToDependencies();
+	
+	if (AbilitySystemComponent)
+	{
+		// 1. Conectamos nuestro "oído" al Ability System Component del jugador
+		AbilitySystemComponent->OnActiveGameplayEffectAddedDelegateToSelf.AddUObject(this, &UOverlayWidgetController::OnGameplayEffectApplied);
+	}
 	const UAuraAttributeSet* AuraAttributeSet = CastChecked<UAuraAttributeSet>(AttributeSet);
 
 	// Usamos FOnAttributeChangeData (Este es el nombre real de la estructura)
@@ -32,4 +40,33 @@ void UOverlayWidgetController::BindCallbacksToDependencies()
 				OnMaxHealtChanged.Broadcast(Data.NewValue);
 			}
 		);
+}
+
+void UOverlayWidgetController::OnGameplayEffectApplied(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	// 1. Extraemos las etiquetas descriptivas (Asset Tags)
+	FGameplayTagContainer AssetTags;
+	SpecApplied.GetAllAssetTags(AssetTags);
+
+	// 2. Extraemos las etiquetas que se le pegan al jugador (Granted Tags)
+	FGameplayTagContainer GrantedTags;
+	SpecApplied.GetAllGrantedTags(GrantedTags);
+
+	// 3. Juntamos ambas listas en una sola para no perder ninguna
+	FGameplayTagContainer AllTags;
+	AllTags.AppendTags(AssetTags);
+	AllTags.AppendTags(GrantedTags);
+
+	// 4. Ahora sí, revisamos la lista completa
+	for (const FGameplayTag& Tag : AllTags)
+	{
+		// Buscamos si la etiqueta contiene la palabra "Cooldown"
+		if (Tag.ToString().Contains("Cooldown"))
+		{
+			float Duration = SpecApplied.GetDuration();
+			
+			// Le avisamos al Blueprint
+			OnCooldownChanged.Broadcast(Tag, Duration, Duration);
+		}
+	}
 }
--- Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/Aura/Public/UI/WidgetController/OverlayWidgetController.h
@@ -4,10 +4,14 @@
 
 #include "CoreMinimal.h"
 #include "UI/WidgetController/AuraWidgetController.h"
+#include "GameplayTagContainer.h"
+#include "GameplayEffect.h"
 #include "OverlayWidgetController.generated.h"
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealtChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealtChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(FOnCooldownChangedSignature, FGameplayTag, CooldownTag, float, TimeRemaining, float, Duration);
+
 
 /**
  * 
@@ -26,4 +30,12 @@ public:
 	FOnMaxHealtChangedSignature OnMaxHealtChanged;
 	
 	virtual void BindCallbacksToDependencies() override;
+	
+	// Este es el evento rojo que aparecerá en Blueprint
+    	UPROPERTY(BlueprintAssignable, Category="Aura|UI")
+    	FOnCooldownChangedSignature OnCooldownChanged;
+    	
+    	protected:
+        	//Esta función se disparará automáticamente cuando te apliquen cualquier efecto
+        	void OnGameplayEffectApplied(UAbilitySystemComponent* TargetASC, const FGameplayEffectSpec& SpecApplied, FActiveGameplayEffectHandle ActiveEffectHandle);
 };
